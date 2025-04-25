416639ac151af6162737a12636f639db933eeb19
Eli Kobia <kobiaeli04@gmail.com>
2025-04-25 13:49:37 +0300

    "Moved FirebaseConfig.ts to new firebase folder and renamed file to firebaseConfig.ts"

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +19,9 @@ const firebaseConfig = {
   measurementId: "G-BYNY9QHJNC"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth();
+auth.languageCode = 'it';
+
+export { app, analytics, auth };
